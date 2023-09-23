de010fee8bbf62ab55079aa1acd91d523dc2c4cb
Rafael Alves <rafaelpereiraalves@ufba.br>
2023-09-23 15:56:36 -0300

    vamos

--- src/users/activities.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './activities.service';
-import { ActivitiesController } from './activities.controller';
-
-@Module({
-  controllers: [ActivitiesController],
-  providers: [UsersService],
-})
-export class ActivitiesModule {}
